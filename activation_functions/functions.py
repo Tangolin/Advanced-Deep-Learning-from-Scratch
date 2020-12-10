7251e9a61dda2a88895b4f230ee2a5f638381303
LeonGur <45076562+LeonGur@users.noreply.github.com>
2020-12-10 15:54:48 +0100

    everything implemented (or at least the exo)

--- activation_functions/functions.py
+++ activation_functions/functions.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""
+Allows for scalar and vector inputs (some also for matrix)
+"""
 
 def sigmoid(x):
     return 1/ (1+np.exp(-x))
@@ -23,14 +26,6 @@ def SiLU(x):
 
 def d_SiLU(x):
     return sigmoid(x) + x * d_sigmoid(x)
-
-def ReLU(x):
-    return np.max(np.c_[np.zeros((np.shape(x)[0])), x], axis=1)
-
-def d_ReLU(x):
-    x[np.where(x<=0)] = 0
-    x[np.where(x>0)] = 1
-    return x
 
 def tanh(x):
     return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
@@ -59,6 +54,98 @@ def soft_sign(x):
 def d_soft_sign(x):
     return x / (np.abs(x)+1)**2
 
+def ReLU(x):
+    return np.max(np.c_[np.zeros((np.shape(x)[0])), x], axis=1)
+
+def d_ReLU(x):
+    x[np.where(x<=0)] = 0
+    x[np.where(x>0)] = 1
+    return x
+
+def LReLU(x, a=0.01):
+    # Leaky ReLU
+    x[np.where(x<=0)] = a*x[np.where(x<=0)]
+    return x
+
+def d_LReLU(x, a=0.01):
+    x[np.where(x>0)] = 1
+    x[np.where(x<=0)] = a
+    return x
+def PReLU(x):
+    # Parametric Rectified Linear Units
+    #### learnable parameter is slightly harder to implement, eh
+    return x
+
+def d_PReLU(x):
+    # same walao
+    return x
+
+def RPReLU(x):
+    # Randomized Leaky Recitified Linear Units
+    ### ayio, learning parameter
+    return x
+
+def d_RPReLU(x):
+    # same eh
+    return x
+
+def SReLU(x):
+    # S-shaped Rectified Linear Units
+    #same learning
+    return x
+
+def d_SReLU(x):
+    # learned parameters
+    return x
+
+def softplus(x):
+    return np.log(1+np.exp(x))
+
+def d_softplus(x):
+    return np.exp(x) / (1+np.exp(x))
+
+def ELUs(x, a=1):
+    # Exponential Linear Units
+    x[np.where(x<=0)] = a*np.exp(x[np.where(x<=0)])-1
+    return x
+
+def d_ELUs(x, a=1):
+    x[np.where(x>0)] = 1
+    x[np.where(x<=0)] = ELUs(x[np.where(x<=0)].copy()) + a
+    return x
+
+def PELU(x, a=1, b=1, c=1):
+    # Parametric Exponential Linear Unit                        ##### not working (1,1,1) should return ELU
+    x[np.where(x>0)] = c*x[np.where(x>0)]
+    x[np.where(x<=0)] = a*np.exp(x[np.where(x<=0)]/b)
+    return x
+
+def d_PELU(x, a, b, c):
+    #
+    pass
+
+def SELU(x, a=1.6733, r=1.0507):
+    # Scaled Exponential Linear Units
+    x[np.where(x<=0)] = a*np.exp(x[np.where(x<=0)])-a
+    return r*x
+def d_SELU(x, a=1.6733, r=1.0507):
+    x[np.where(x>0)] = 1
+    x[np.where(x<=0)] = a*r*np.exp(x[np.where(x<=0)])
+    return x
+
+def maxount(x):
+    # requires a network for implementation
+    return x
+
+def d_maxount(x):
+    return x
+
+def swish(x):
+    return x * sigmoid(x)
+
+def d_swish(x):
+    return sigmoid(x) * (1+x*(1-sigmoid(x))) #double check that maybe, but I think my math is ok
+
 def plot(func, start=-5, end=5, step=0.05, hold_on=False):
     x = np.arange(start,end,0.05)
     plt.plot(x, func(x.copy()))
@@ -67,5 +154,5 @@ def plot(func, start=-5, end=5, step=0.05, hold_on=False):
 
 
 
-plot(soft_sign, hold_on=True)
-plot(d_soft_sign)
+plot(swish, hold_on=True)
+plot(d_swish)
